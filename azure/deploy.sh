53a08f1081054b57ba8b9b60b589df883956f569
Joachim Aumann <joachimaumann@Joachims-MacBook-Air.local>
2017-07-10 23:54:48 +0200

    added node version and parameterized the branch

---
+++ azure/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+gitrepo=https://github.com/daumann/chronas.git
+token=<fillInTOken>
+webappname=chronasNew$RANDOM
+branch=dev
+
+
+# Create a resource group.
+az group create --location westeurope --name myResourceGroup
+
+# Create an App Service plan in `FREE` tier.
+az appservice plan create --name $webappname --resource-group myResourceGroup --sku FREE
+
+# Create a web app.
+az webapp create --name $webappname --resource-group myResourceGroup --plan $webappname
+
+# Configure continuous deployment from GitHub. 
+# --git-token parameter is required only once per Azure account (Azure remembers token).
+az webapp deployment source config --name $webappname --resource-group myResourceGroup \
+--repo-url $gitrepo --branch $branch --git-token $token
+
+# Browse to the web app.
+az webapp browse --name $webappname --resource-group myResourceGroup
