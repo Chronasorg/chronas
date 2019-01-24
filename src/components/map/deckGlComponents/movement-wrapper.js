14fb7222f990dbf3c1c961d15ec24eb48692e97b
Dietmar Aumann <dietmar.aumann@gmail.com>
2019-01-23 23:57:38 -0600

    preperformance

--- src/components/map/deckGlComponents/movement-wrapper.js
+++ src/components/map/deckGlComponents/movement-wrapper.js
@@ -1,189 +1,97 @@
-import {Layer} from 'deck.gl';
+/* eslint-disable max-len */
+import { CompositeLayer } from 'deck.gl'
+import MovementLayer from './movement-layer'
 
-import {Model, Geometry} from 'luma.gl';
-
-const tripsVertex = `\
-#define SHADER_NAME trips-layer-vertex-shader
-
-attribute vec3 positions;
-attribute vec3 colors;
-
-uniform float opacity;
-uniform float currentTime;
-uniform float trailLength;
-
-varying float vTime;
-varying vec4 vColor;
-
-void main(void) {
-  vec2 p = project_position(positions.xy);
-  // the magic de-flickering factor
-  vec4 shift = vec4(0., 0., mod(positions.z, trailLength) * 1e-4, 0.);
-
-  gl_Position = project_to_clipspace(vec4(p, 1., 1.)) + shift;
-
-  vColor = vec4(colors / 255.0, opacity);
-  vTime = 1.0 - (currentTime - positions.z) / trailLength;
-}
-`;
-
-const tripsFragment = `\
-#define SHADER_NAME trips-layer-fragment-shader
-
-precision highp float;
-
-varying float vTime;
-varying vec4 vColor;
-
-void main(void) {
-  if (vTime > 1.0 || vTime < 0.0) {
-    discard;
-  }
-  gl_FragColor = vec4(vColor.rgb, vColor.a * vTime);
-}
-`;
-
+const FULLTIMEINTERVAL = 1000
+const lightAmount = 4
 const defaultProps = {
-  trailLength: 120,
-  currentTime: 0,
-  getPath: d => d.path,
-  getColor: d => d.color
-};
+}
 
-export default class MovementLayer extends Layer {
-  initializeState() {
-    const {gl} = this.context;
-    const attributeManager = this.getAttributeManager();
+let _yearInitialized = ''
+let _animationFrame = false
+export default class MovementLayerWrapper extends CompositeLayer {
+  initializeState () {
+    this.finalizeState()
+    console.debug("!!-!! initialize")
+    this.state = {
+      currentYear: '',
+      // Cached tags per zoom level
+      time: FULLTIMEINTERVAL
+    }
+    const movementData = []
+    const sliceTime = FULLTIMEINTERVAL / lightAmount
+    this.props.preData.data.forEach(el => {
+      if (el[0].toString() !== el[1].toString()) {
+        // const distance = Math.abs(el[0][0] - el[1][0]) + Math.abs(el[0][1] - el[1][1])
+        // console.debug(distance, Math.floor(distance/12) + 1)
+        // lightAmount = Math.floor(distance/12) + 2
+        for (var i = 1; i < lightAmount -2; i++) {
+          movementData.push([
+            [el[0][0], el[0][1], -FULLTIMEINTERVAL +(i*sliceTime)],
+            [el[1][0], el[1][1], i*sliceTime],
+          ])
+        }
+      }
+    })
+    console.debug("!!-!! movementData", movementData.length)
+    this.setState({
+      movementData
+    })
+    this._animate();
+  }
 
-    const model = this.getModel(gl);
+  shouldUpdateState ({ props, oldProps, changeFlags }) {
+    if (changeFlags.dataChanged) {
+      console.error("dataChanged!")
+    }
+    if (props.year !== oldProps.year && _yearInitialized !== props.year /*(JSON.stringify(props.preData.data[0] || {}) !== JSON.stringify(oldProps.preData.data[0] || {}))*/) {
+      // this.i
+      this.finalizeState()
+      _yearInitialized = props.year
+      this.initializeState ()
+    }
+    return changeFlags.somethingChanged
+  }
 
-    attributeManager.add({
-      indices: {size: 1, update: this.calculateIndices, isIndexed: true},
-      positions: {size: 3, update: this.calculatePositions},
-      colors: {size: 3, update: this.calculateColors}
+  _animate() {
+    const {
+      loopLength = FULLTIMEINTERVAL, // unit corresponds to the timestamp in source data
+      animationSpeed = FULLTIMEINTERVAL/10 // unit time per second
+    } = this.props;
+    const timestamp = Date.now() / 1000;
+    const loopTime = loopLength / animationSpeed;
+
+    this.setState({
+      time: ((timestamp % loopTime) / loopTime) * loopLength
     });
-
-    gl.getExtension('OES_element_index_uint');
-    this.setState({model});
+    // console.debug(this.state)
+    _animationFrame = window.requestAnimationFrame(this._animate.bind(this));
   }
 
-  updateState({props, changeFlags: {dataChanged}}) {
-    if (dataChanged) {
-      this.countVertices(props.data);
-      this.state.attributeManager.invalidateAll();
+  finalizeState() {
+    if (this._animationFrame) {
+      window.cancelAnimationFrame(this._animationFrame);
+      _animationFrame = false
+      _yearInitialized = false
     }
   }
 
-  getModel(gl) {
-    return new Model(gl, {
-      id: this.props.id,
-      vs: tripsVertex,
-      fs: tripsFragment,
-      geometry: new Geometry({
-        id: this.props.id,
-        drawMode: 'LINES'
-      }),
-      vertexCount: 0,
-      isIndexed: true,
-      // TODO-state-management: onBeforeRender can go to settings, onAfterRender, we should
-      // move this settings of corresponding draw.
-      onBeforeRender: () => {
-        gl.enable(gl.BLEND);
-        gl.enable(gl.POLYGON_OFFSET_FILL);
-        gl.polygonOffset(2.0, 1.0);
-        gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
-        gl.blendEquation(gl.FUNC_ADD);
-      },
-      onAfterRender: () => {
-        gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-        gl.disable(gl.POLYGON_OFFSET_FILL);
-      }
-    });
-  }
-
-  countVertices(data) {
-    if (!data) {
-      return;
-    }
-
-    const {getPath} = this.props;
-    let vertexCount = 0;
-    const pathLengths = data.reduce((acc, d) => {
-      const l = getPath(d).length;
-      vertexCount += l;
-      return [...acc, l];
-    }, []);
-    this.setState({pathLengths, vertexCount});
-  }
-
-  draw({uniforms}) {
-    const {trailLength, currentTime} = this.props;
-    this.state.model.render(
-      Object.assign({}, uniforms, {
-        trailLength,
-        currentTime
-      })
-    );
-  }
-
-  calculateIndices(attribute) {
-    const {pathLengths, vertexCount} = this.state;
-
-    const indicesCount = (vertexCount - pathLengths.length) * 2;
-    const indices = new Uint32Array(indicesCount);
-
-    let offset = 0;
-    let index = 0;
-    for (let i = 0; i < pathLengths.length; i++) {
-      const l = pathLengths[i];
-      indices[index++] = offset;
-      for (let j = 1; j < l - 1; j++) {
-        indices[index++] = j + offset;
-        indices[index++] = j + offset;
-      }
-      indices[index++] = offset + l - 1;
-      offset += l;
-    }
-    attribute.value = indices;
-    this.state.model.setVertexCount(indicesCount);
-  }
-
-  calculatePositions(attribute) {
-    const {data, getPath} = this.props;
-    const {vertexCount} = this.state;
-    const positions = new Float32Array(vertexCount * 3);
-
-    let index = 0;
-    for (let i = 0; i < data.length; i++) {
-      const path = getPath(data[i]);
-      for (let j = 0; j < path.length; j++) {
-        const pt = path[j];
-        positions[index++] = pt[0];
-        positions[index++] = pt[1];
-        positions[index++] = pt[2];
-      }
-    }
-    attribute.value = positions;
-  }
-
-  calculateColors(attribute) {
-    const {data, getColor} = this.props;
-    const {pathLengths, vertexCount} = this.state;
-    const colors = new Float32Array(vertexCount * 3);
-
-    let index = 0;
-    for (let i = 0; i < data.length; i++) {
-      const color = getColor(data[i]);
-      const l = pathLengths[i];
-      for (let j = 0; j < l; j++) {
-        colors[index++] = color[0];
-        colors[index++] = color[1];
-        colors[index++] = color[2];
-      }
-    }
-    attribute.value = colors;
+  renderLayers () {
+    const { trailLength = 800, year } = this.props
+    const { movementData, time } = this.state
+    return new MovementLayer({
+      id: 'movement',
+      data: movementData,
+      currentTime: time,
+      getPath: d => d,
+      opacity: 1,
+      strokeWidth: 20,
+      year,
+      getColor: () => [32, 74, 0], // (d.vendor === 0 ? [253, 128, 93] : [80, 0, 0]
+      trailLength,
+    })
   }
 }
 
-MovementLayer.layerName = 'MovementLayer';
-MovementLayer.defaultProps = defaultProps;
+MovementLayerWrapper.layerName = 'MovementLayerWrapper'
+MovementLayerWrapper.defaultProps = defaultProps
