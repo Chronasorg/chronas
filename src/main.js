3196120670255a8140dba908c2b3100149ed29cf
veste-software <66267130+veste-software@users.noreply.github.com>
2022-02-14 13:45:02 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import createStore from './store/createStore'
-import 'react-image-gallery/styles/scss/image-gallery.scss'
 import './styles/main.scss'
 import './styles/videoplayer.scss'
 import './styles/login.scss'
